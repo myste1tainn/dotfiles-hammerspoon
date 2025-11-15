910b01e418b0e971ce204d3b66a28a3f0b9e93bb
Arnon Keereena <arnon.keereena@accenture.com>
2025-11-15 14:54:01 +0700

    WIP: Reorganize and add rule for Karabiner-Element control
    - Organize config and codes
    - WIP: Added Karabiner-Element code and config structure
    
    Next: Complete the code that regenerate config depends on the active
    input source and reload Karabiner-Element

---
+++ lua/aerospace/change_config_based_on_input_source.lua
@@ -0,0 +1,34 @@
+---- This script manages keyboard layouts and monitors in a macOS environment using Hammerspoon
+--- to allow the aeruspace shortcut to reflect the current keyboard layout, since aerospace maps the key
+--- literals to the current keyboard layout, and it is not possible to change the layout in aerospace, dynamically
+--- without having to edit the config file manually.
+local function rebuildAeroConfig(layout)
+	local basePath = os.getenv("HOME") .. "/aerospace-configs/"
+	local layoutFile = basePath .. "layouts/" .. layout .. ".toml"
+	local baseFile = basePath .. "base.toml"
+	local targetFile = os.getenv("HOME") .. "/.aerospace.toml"
+
+	local cmd = string.format('cat "%s" "%s" > "%s"', baseFile, layoutFile, targetFile)
+
+	hs.execute(cmd)
+	hs.execute("/opt/homebrew/bin/aerospace reload-config")
+end
+
+local function onLayoutChanged()
+	local layoutId = hs.keycodes.currentSourceID()
+	if layoutId:find("Dvorak") then
+		rebuildAeroConfig("dvorak")
+	else
+		rebuildAeroConfig("qwerty")
+	end
+end
+
+-- Attach to input source change event
+hs.keycodes.inputSourceChanged(onLayoutChanged)
+
+hs.hotkey.bind({ "cmd", "alt" }, "C", function()
+	local win = hs.window.focusedWindow()
+	if win then
+		win:centerOnScreen()
+	end
+end)
