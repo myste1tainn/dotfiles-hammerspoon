5e2ef6c7df41ac2f8f8dc0c785b23f1628dab498
Arnon Keereena <arnon.keereena@accenture.com>
2026-04-01 20:38:25 +0700

    Add emacs key bindings capability, changes per layout Dvorak or Qwerty

--- Spoons/Karabiner.spoon/emacs_key_bindings.lua
+++ Spoons/Karabiner.spoon/emacs_key_bindings.lua
@@ -45,15 +45,28 @@ return {
 			from = { "n", { "control" } },
 			to = { { "down_arrow" } },
 		},
+		-- NOTE: While this works in cell editing in MS Excel, browsing cells doesn't
+		-- {
+		-- 	kind = "movement",
+		-- 	from = { "a", { "control" } },
+		-- 	to = { { "left_arrow", { "fn" } } },
+		-- },
+		-- {
+		-- 	kind = "movement",
+		-- 	from = { "e", { "control" } },
+		-- 	to = { { "right_arrow", { "fn" } } },
+		-- },
+		-- NOTE: While this works in both cell editing and browsing in MS Excel, cells editing works like word jump not Home/End
+		-- TODO: Need to make a different rule set for Excel, which then means for these 2 only it will have to be excluded from the shared_conditions above
 		{
 			kind = "movement",
 			from = { "a", { "control" } },
-			to = { { "home" } },
+			to = { { "left_arrow", { "command" } } },
 		},
 		{
 			kind = "movement",
 			from = { "e", { "control" } },
-			to = { { "end" } },
+			to = { { "right_arrow", { "command" } } },
 		},
 		{
 			kind = "movement",
@@ -78,7 +91,9 @@ return {
 		{
 			kind = "editing",
 			from = { "w", { "control" } },
-			to = { { "delete_or_backspace", { "option" } } },
+			-- This won't work in MS Excel, so we cheat a bit instead
+			-- to = { { "delete_or_backspace", { "option" } } },
+			to = { { "left_arrow", { "shift", "option" } }, { "delete_or_backspace" } },
 		},
 		{
 			kind = "editing",
@@ -93,12 +108,12 @@ return {
 		{
 			kind = "editing",
 			from = { "k", { "control" } },
-			to = { { "right_arrow", { "shift", "command" } }, { "delete_or_backspace" } },
+			to = { { "right_arrow", { "shift", "fn" } }, { "delete_or_backspace" } },
 		},
 		{
 			kind = "editing",
 			from = { "u", { "control" } },
-			to = { { "left_arrow", { "shift", "command" } }, { "delete_or_backspace" } },
+			to = { { "left_arrow", { "shift", "fn" } }, { "delete_or_backspace" } },
 		},
 	},
 }
